f3bb8672e181150589950eba87f29615a95e9f69
webdev <webdev@PROGRAMMER>
2016-11-23 12:24:46 +0500

    Сделан делегат

--- main.cpp
+++ main.cpp
@@ -3,124 +3,12 @@
 
 #include <assert.h>
 
-//   онтейнер дл€ хранени€ до 2-х аргументов.
-struct NIL {};
-class IArguments { public: virtual ~IArguments() {} };
-template< class T1 = NIL, class T2 = NIL >
-class Arguments: public IArguments
-{
-public:
-	Arguments() {}
-	Arguments(T1 i_arg1): arg1(i_arg1) {}
-	Arguments(T1 i_arg1, T2 i_arg2): arg1(i_arg1), arg2(i_arg2) {}
-	T1 arg1;
-	T2 arg2;
-};
-
-//   онтейнер дл€ хранени€ указател€ на метод.
-class IContainer
-{
-public:
-	virtual void Call(IArguments*) = 0;
-};
-template< class T, class M > class Container: public IContainer {};
-
-//  —пециализаци€ дл€ метода без аргументов.
-template< class T >
-class Container< T, void (T::*)(void) >: public IContainer
-{
-	typedef void (T::*M)(void);
-public:
-	Container(T* c, M m): m_class(c), m_method(m) {}
-private:
-	T* m_class;
-	M m_method;
-public:
-	void Call(IArguments* i_args)
-	{
-		(m_class->*m_method)();
-	}
-};
-
-//  —пециализаци€ дл€ метода с одним аргументом.
-template< class T, class A1 >
-class Container< T, void (T::*)(A1) >: public IContainer
-{
-	typedef void (T::*M)(A1);
-	typedef Arguments<A1> A;
-	public:
-		Container(T* c, M m): m_class(c), m_method(m) {}
-	private:
-		T* m_class;
-		M m_method;
-	public:
-		void Call(IArguments* i_args)
-		{
-			A* a = dynamic_cast< A* >(i_args);
-			assert(a);
-			if(a) (m_class->*m_method)(a->arg1);
-		}
-};
-
-//  —пециализаци€ дл€ метода с двум€ аргументами
-template< class T, class A1, class A2 >
-class Container< T, void (T::*)(A1, A2) >: public IContainer
-{
-	typedef void (T::*M)(A1, A2);
-	typedef Arguments<A1, A2> A;
-	public:
-		Container(T* c, M m): m_class(c), m_method(m) {}
-	private:
-		T* m_class;
-		M m_method;
-	public:
-		void Call(IArguments* i_args)
-		{
-			A* a = dynamic_cast< A* >(i_args);
-			assert(a);
-			if(a) (m_class->*m_method)(a->arg1, a->arg2);
-		}
-};
-
-//  —обственно делегат.
-class Delegate
-{
-public:
-
-	Delegate(): m_container(0) {}
-	~Delegate() { if(m_container) delete m_container; }
-
-	template< class T, class U > void Connect(T* i_class, U i_method)
-	{
-		if(m_container) delete m_container;
-		m_container = new Container< T, U >(i_class, i_method);
-	}
-
-	void operator()()
-	{
-		m_container->Call(&Arguments<>());
-	}
-
-	template< class T1 > void operator()(T1 i_arg1)
-	{
-		m_container->Call(&Arguments< T1 >(i_arg1));
-	}
-
-	template< class T1, class T2 > void operator()(T1 i_arg1, T2 i_arg2)
-	{
-		m_container->Call(&Arguments< T1, T2 >(i_arg1, i_arg2));
-	}
-
-private:
-	IContainer* m_container;
-};
-
 struct Int
 {
 	int i = 0;
-	void Add()
+	void Add(int x, int y )
 	{
-		i = 5;
+		i = x + y;
 	}
 	int Get()
 	{
@@ -128,53 +16,76 @@ struct Int
 	}
 };
 
-
-class IClass
-{
-public:
-	virtual void Call() = 0;
-};
-
-template< class C>
-class Clas: public IClass
-{
-private:
-public:
-	C m_class;
-	Clas(C c): m_class(m) {}
-};
 //
 //  онтейнер дл€ хранени€ указател€ на метод.
+class IArgs { public: virtual ~IArgs() {} };
+template<typename... Args>
+class Argums: public IArgs
+{
+public:
+	std::tuple<Args...> arg;
+	Argums(Args... args)
+	{
+		arg = make_tuple(args...);
+	}
+};
 class ICont
 {
 public:
-	virtual void Call(char *v, ...) = 0;
+	virtual void Call(char *v, IArgs* i_args) = 0;
 };
 
-template<class M>
-class ContV: public ICont
+namespace detail
+{
+	template <typename C, typename M, typename Tuple, bool Done, int Total, int... N>
+	struct call_impl
+	{
+		static void call(C c, M m, Tuple && t)
+		{
+			call_impl<C, M, Tuple, Total == 1 + sizeof...(N), Total, N..., sizeof...(N)>::call(c, m, std::forward<Tuple>(t));
+		}
+	};
+
+	template <typename C, typename M, typename Tuple, int Total, int... N>
+	struct call_impl<C, M, Tuple, true, Total, N...>
+	{
+		static void call(C c, M m, Tuple && t)
+		{
+			(c->*m)(std::get<N>(std::forward<Tuple>(t))...);
+		}
+	};
+}
+
+// user invokes this
+template <typename C, typename M, typename Tuple>
+void call(C c, M m, Tuple && t)
+{
+	typedef typename std::decay<Tuple>::type ttype;
+	detail::call_impl<C, M, Tuple, 0 == std::tuple_size<ttype>::value, std::tuple_size<ttype>::value>::call(c, m, std::forward<Tuple>(t));
+}
+
+template<class...>struct types { using type = types; };
+template<class Sig> struct args;
+template<class R, class...Args>
+struct args<R(Args...)>:types<Args...> {};
+template<class Sig> using args_t = typename args<Sig>::type;
+
+
+template<typename C, typename M, typename... Args>
+class ContA: public ICont
 {
 private:
 	M m_method;
+	typedef Argums<Args...> A;
+	//	typename result_of<decltype(m_method)(C)>::type val;
 public:
-	ContV(M m): m_method(m) {}
-	void Call(char *v, ...)
+	ContA(M m): m_method(m) {}
+	void Call(char *v, IArgs* i_args)
 	{
-		(*m_method)();
-	}
-};
-
-template<class C, class M>
-class Cont: public ICont
-{
-private:
-	M m_method;
-//	typename result_of<decltype(m_method)(C)>::type val;
-public:
-	Cont(M m): m_method(m) {}
-	void Call(char *v, ...)
-	{
-		((C*)v->*m_method)();
+		//args_t<decltype(m_method)>;
+		A* a = dynamic_cast<A*>(i_args);
+		assert(a);
+		if(a) call((C*)v, m_method, a->arg);
 	}
 };
 
@@ -182,25 +93,28 @@ class Del
 {
 	ICont* mCont = nullptr;
 public:
-	template<class C, class M> void Connect(M method)
+	template<class C, typename... Args, class M> void Connect(M method)
 	{
 		if(mCont) delete mCont;
-		mCont = new Cont<C, M>(method);
+		mCont = new ContA<C, M, Args...>(method);
 	}
 	template<class M> void Connect(M method)
 	{
-		if(mCont) delete mCont;
-		mCont = new ContV<M>(method);
+		//if(mCont) delete mCont;
+		//mCont = new ContV<M>(method);
 	}
-	void Call()
+	/*template<typename... Args> void Call(Args... args)
 	{
-		mCont->Call(nullptr);
-	}
+		//mCont->Call(args...);
+	}*/
 	template<typename... Args> void Call(void *v, Args... args)
 	{
-		mCont->Call((char*)v, args...);
+		mCont->Call((char*)v, &Argums<Args...>(args...));
 	}
-};
+}; /*template< class T1, class T2 > void operator()(T1 i_arg1, T2 i_arg2)
+{
+	m_container->Call(&Arguments< T1, T2 >(i_arg1, i_arg2));
+}*/
 
 void Vr()
 {
@@ -210,13 +124,15 @@ void Vr()
 int main()
 {
 	//void *v = Int::Add;
+	int x = 6;
 	Int id;
 	Del d;
-	d.Connect<Int>(&Int::Add);
-	d.Call(&id, 5, 8, 5);
-
-	d.Connect(&Vr);
-	d.Call();
+	d.Connect<Int, int, int>(&Int::Add);
+	d.Call(&id, x, 6);
+	//d.Connect<Int, int(__thiscall Int::*)()>(&Int::Get);
+	cout << x << endl;
+	//d.Connect(&Vr);
+	//d.Call();
 
 	cout << id.i;
 	string code = "int i = 5, v = 6 + 2; i = 8;";
